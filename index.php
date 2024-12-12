89ab7fc0b864ebcae87716d2f9aa2d2a2df0cc0b
Khai Thieu <khaithieu3@gmail.com>
2024-12-12 00:29:47 -0600

    Added a basic nav

--- index.php
+++ index.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OpenDiary</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <title>OPEN DIARY</title>
 </head>
 <body>
-    
+    <nav>
+        <h1>OPEN DIARY</h1>
+        <span class="material-icons">search</span>
+        <span class="material-icons">account_circle</span>
+    </nav>
 </body>
 </html>
