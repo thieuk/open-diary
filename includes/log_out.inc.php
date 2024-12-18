63d6184e58ebb0addac299c18b33f66cbd389280
Khai Thieu <khaithieu3@gmail.com>
2024-12-18 01:12:18 -0600

    change log_out.inc.php path

--- includes/log_out.inc.php
+++ includes/log_out.inc.php
@@ -1,15 +1,13 @@
 <?php 
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    require_once "config_session.inc.php";
-
     session_unset();
     session_destroy();
 
-    header("Location: ../../login.php");
+    header("Location: ../login.php");
     die();
 }
 else {
-    header("Location: ../../index.php");
+    header("Location: ../index.php");
     die();
 }
