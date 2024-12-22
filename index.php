7f2b0296012e102ed2439b8a72045ab38737af18
Khai Thieu <khaithieu3@gmail.com>
2024-12-21 22:04:00 -0600

    style home page

--- index.php
+++ index.php
@@ -11,5 +11,14 @@
     <?php 
         require_once("nav.php"); 
     ?>
+
+    <div id="main-container">
+        <img src="img/diary.png" alt="book and pencils">
+        <div>
+            <h1>Why I Created This Space</h1>
+            <p>During my last semester of college, I experienced a one-sided love. I was in a group with a guy, NK, for a class project, but I didn't realize my feelings for him until our final day of class on Wednesday, December 4, 2024. The thought of never seeing him again broke my heart, and I poured my emotions into my notes app. Overwhelmed and unsure of how to cope, I considered sharing my feelings online but hesitated, fearing judgment. That's when the idea for this website came to me: a safe space where anyone can share their thoughts and feelings without fear of being judged.</p>
+            <a href="entry.php">See What Others Have Shared</a>
+        </div>
+    </div>
 </body>
 </html>
